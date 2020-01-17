a283074552d1bd5ae6cb42be95af2ecb9f2fd39f
gianluca <gianluca.riboldi@telnext.com>
2020-01-17 18:42:41 +0100

    [Activity] stop

--- routes/activity.js
+++ routes/activity.js
@@ -119,3 +119,11 @@ exports.validate = function (req, res) {
   logData(req);
   res.send(200, 'Validate');
 };
+
+exports.stop = function (req, res) {
+  // Data from the req and put it in an array accessible to the main app.
+  //console.log( req.body );
+  console.log('Stop')
+  logData(req);
+  res.send(200, 'Stop');
+};
